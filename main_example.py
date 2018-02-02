c4878a1e8330fd79f243b4cc43dcd6f922aeb423
JanLuRe <jan.reubold@tu-dresden.de>
2018-02-02 11:53:29 +0100

    main_example: most basic example

--- main_example.py
+++ main_example.py
@@ -7,8 +7,8 @@ import numpy as np
 # Note: train_test_split is missing in this mini-example --> results are not representative!!!
 
 # DATA
-# X, Xd, y = ToyData.sample(1000, durations=False)
-X, Xd, y = ToyData.sample(1000, durations=True, udist=[.25, .25, .25, .25], dmean=[10, 20, 10, 40], dvar=[1.0, 1.0, 1.0, 1.0])
+X, Xd, y = ToyData.sample(100, durations=False)
+#X, Xd, y = ToyData.sample(1000, durations=True, udist=[.25, .25, .25, .25], dmean=[10, 20, 10, 40], dvar=[1.0, 1.0, 1.0, 1.0])
 
 # for grid-search
 C_range = np.logspace(-2, 10, 13)
@@ -20,8 +20,8 @@ par = dict(gamma=gamma_range, C=C_range, class_weight=['balanced'])
 X_flat = [session for user in X for session in user]
 Xd_flat = [session for user in Xd for session in user] if Xd is not None else None
 print('Fitting Segmentation Model...')
-gpa = GlobalPatternAnalysis(X_flat, Xd_flat, L=20, num_it=500, eval_intervals=[(100, 250), (250, 490), (450, 500)], model_save_path='model_example_4c',
-                            verbose=True)
+gpa = GlobalPatternAnalysis(X_flat, Xd_flat, L=20, num_it=200, eval_intervals=[(100, 150), (150, 200)], model_save_path='model_example_3c_simple',
+                            verbose=False)
 
 # transform original data into vector-space representation
 print('Transforming Data...')
